e6a853795b755b398df0d9155c51938f8fd02020
Gene <gkr6sy@virginia.edu>
2015-02-23 17:53:40 -0500

    creates basic django template with Oscar tutorial done up to 'Creating product classes and fulfillment partners'

---
+++ slamjam/slamjam/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from oscar.app import application
+
+urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include(application.urls)),
+]
+
