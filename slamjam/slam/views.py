11ee31c9c31f92e74acfd341c25eb0978734722d
Gene <gkr6sy@virginia.edu>
2015-03-01 17:00:57 -0500

    added basic css and html

--- slamjam/slam/views.py
+++ slamjam/slam/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse("Welcome to the SlamJam Homepage")
+    return render(request, 'slam/index.html')
