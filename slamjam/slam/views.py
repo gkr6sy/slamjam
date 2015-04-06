2ef6556e9cb74ad4dead7529a8d9cfeb612fe3ee
Joseph <jcu5df@virginia.edu>
2015-04-06 18:48:17 -0400

    User Signup

--- slamjam/slam/views.py
+++ slamjam/slam/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .forms import SignupForm
 # Create your views here.
 
 
@@ -8,3 +10,13 @@ def home(request):
     return render(request, 'slam/index.html')
 def about(request):
     return render(request, 'slam/about.html')
+
+def signup(request):
+    if request.method == 'POST':
+       form = SignupForm(request.POST)
+       if form.is_valid():
+          user = User.objects.create_user(form['username'].value(), (form['username'].value(), form['password'].value())
+          return HttpResponseRedirect('/')
+    else:
+       form = SignupForm()
+    return render(request, 'slam/signup.html', {'form':form}
