4138f06aac18595b8cbeaefdfb7c652dfb6596fe
Gene <gkr6sy@virginia.edu>
2015-04-06 20:45:56 -0400

    fixed the logout issue and now signups will automatically login when registered

--- slamjam/slam/urls.py
+++ slamjam/slam/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^about/$',views.about,name='about'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.exit, name='logout'),
 )
--- slamjam/slam/views.py
+++ slamjam/slam/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as auth_login
 from .forms import SignupForm, LoginForm
 from django.http import HttpResponseRedirect
@@ -10,29 +10,44 @@ from django.http import HttpResponseRedirect
 
 def home(request):
     return render(request, 'slam/index.html')
+
+
 def about(request):
     return render(request, 'slam/about.html')
+
+
 def signup(request):
     if request.method == 'POST':
-       form = SignupForm(request.POST)
-       if form.is_valid():
-          user = User.objects.create_user(form['username'].value(), form['username'].value(), form['password'].value())
-          return HttpResponseRedirect('/')
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(form['username'].value(), form['username'].value(), form['password'].value())
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            auth_login(request, user)
+            print("Successfully logged in as " + user.username)
+            return HttpResponseRedirect('/')
     else:
-       form = SignupForm()
+        form = SignupForm()
     return render(request, 'slam/signup.html', {'form':form})
+
+
 def login(request):
     if request.method == 'POST':
-      form = LoginForm(request.POST)
-      if form.is_valid():
-         username = request.POST['username']
-         password = request.POST['password']
-         user = authenticate(username = username, password = password)
-         if user is not None:
-            auth_login(request, user)
-            return HttpResponseRedirect('/')
-         else:
-            print("Invalid Login")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                print("Invalid Login")
     else:
-      form = LoginForm()
-    return render(request, 'slam/login.html', {'form':form}) 
+        form = LoginForm()
+    return render(request, 'slam/login.html', {'form':form})
+
+
+def exit(request):
+    logout(request)
+    print("Successfully logged out.")
+    return HttpResponseRedirect('/')
